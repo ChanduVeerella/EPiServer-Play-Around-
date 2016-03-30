cb29f40d7871efc28ac3e3de8e3c3ae93a1c7a1e
Ravichandra Veerella <chandu.veerella548@gmail.com>
2016-03-30 11:50:45 +0530

    Added files via upload
    Sample files for Scheduling of Jobs in Episerver

---
+++ ScheduledJobExample.cs
@@ -0,0 +1,34 @@
+﻿using EPiServer.PlugIn;
+using EPiServer.Scheduler;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Ericsson.com_Team.Schedule_Jobs
+{
+    [ScheduledPlugIn(DisplayName = "ScheduledJobExample")]
+    public class ScheduledJobExample : ScheduledJobBase
+    {
+        private bool _stopSignaled;
+
+        public ScheduledJobExample()
+        {
+            IsStoppable = true;
+        }
+        public override void Stop()
+        {
+            _stopSignaled = true;
+        }
+        public override string Execute()
+        {
+            OnStatusChanged(String.Format("Starting execution of {0}", this.GetType()));
+            if (_stopSignaled)
+            {
+                return "Stop of job was called";
+            }
+
+            return "Job is successfully executed";
+        }
+    }
+}
---
+++ TrashPagesJobSchedule.cs
@@ -0,0 +1,46 @@
+﻿using EPiServer;
+using EPiServer.Core;
+using EPiServer.PlugIn;
+using EPiServer.Scheduler;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Ericsson.com_Team.Schedule_Jobs
+{
+    [ScheduledPlugIn(DisplayName ="Our Sample Scheduled Job", SortIndex =1)]
+    public class TrashPagesJobSchedule : ScheduledJobBase
+    {
+        public override string Execute()
+        {
+            string resultMessage = string.Empty;
+
+            try
+            {
+                // Count the number of pages in the recycle bin 
+                int pagesInRecycleBin =
+                    DataFactory.Instance.GetChildren(
+                    PageReference.WasteBasket).Count;
+
+                // Compose a grammatically correct (!) result message for the job history log 
+                resultMessage =
+                    string.Format(
+                    "There {0} {1} {2} in the recycle bin.",
+                    (pagesInRecycleBin == 1 ? "is" : "are"),
+                    pagesInRecycleBin,
+                    (pagesInRecycleBin == 1 ? "page" : "pages"));
+            }
+            catch
+            {
+                // Error handling goes here 
+
+                resultMessage = "The job could not be completed.";
+            }
+
+            // Return the resulting message (Will be displayed in the scheduled job log) 
+            return resultMessage;
+        }
+         
+    }
+}
